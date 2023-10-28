54a41a51d8ba9ce8d4fc09ef7fffba70a850cb54
Tu Dinh <dinh2644.mail@gmail.com>
2023-10-28 18:12:04 -0400

    completed step 6

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,34 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import "./EditPost.css";
 import { supabase } from "../client";
 
 const EditPost = ({ data }) => {
   const { id } = useParams();
-  const post = data.filter((item) => item.id === id)[0];
+  const post = data.find((item) => String(item.id) === String(id));
+
+  const [editedPost, setEditedPost] = useState({
+    title: "",
+    author: "",
+    description: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEditedPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const updatePost = async (event) => {
+    event.preventDefault();
+
+    const { error } = await supabase
+      .from("Posts")
+      .update({
+        title: editedPost.title,
+        author: editedPost.author,
+        description: editedPost.description,
+      })
+      .eq("id", id);
+
+    if (error) {
+      console.log(error);
+    }
+
+    window.location = "/";
+  };
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <form>
-        <label for="title">Title</label> <br />
-        <input type="text" id="title" name="title" value={post.title} />
+        <label htmlFor="title">Title</label> <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={editedPost.title}
+          onChange={handleChange}
+          placeholder={post.title}
+        />
         <br />
         <br />
-        <label for="author">Author</label>
+        <label htmlFor="author">Author</label>
         <br />
-        <input type="text" id="author" name="author" value={post.author} />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={editedPost.author}
+          onChange={handleChange}
+          placeholder={post.author}
+        />
         <br />
         <br />
-        <label for="description">Description</label>
+        <label htmlFor="description">Description</label>
         <br />
         <textarea
           rows="5"
           cols="50"
           id="description"
-          value={post.description}
+          name="description"
+          value={editedPost.description}
+          placeholder={post.description}
+          onChange={handleChange}
         ></textarea>
         <br />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" onClick={updatePost} />
         <button className="deleteButton">Delete</button>
       </form>
     </div>
